64d0b0033c256c65de8ca1077ae80b33202e67a8
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-12-30 15:27:20 +0000

    Update smoothstate-jquery.js

--- smoothstate-jquery.js
+++ smoothstate-jquery.js
@@ -633,33 +633,7 @@ if(k&&j[k]&&(e||j[k].data)||void 0!==d||"string"!=typeof b)return k||(k=i?a[h]=c
 
 })(jQuery, window, document);
 
+
 ;(function ($) {
-    'use strict';
-    var $body = $('html, body'),
-        $main = $('#main'),
-        options = {
-            prefetch: true,
-            pageCacheSize: 4,
-            onStart: {
-                duration: 250,
-                render: function (url, $container) {
-                    $body.animate({
-                        scrollTop: 0
-                    });
-                    $main.addClass('is-exiting');
-                    smoothState.restartCSSAnimations();
-                }
-            },
-            onEnd: {
-                duration: 0,
-                render: function (url, $container, $content) {
-                    $main.removeClass('is-exiting');
-                    $main.html($content);
-                    $body.css('cursor', 'auto');
-                    $body.find('a').css('cursor', 'auto');
-                }
-            }
-        },
-        smoothState = $main.smoothState(options).data('smoothState');
-    window.smoothstate = smoothState;
+    $('#all').smoothState();
 })(jQuery);
